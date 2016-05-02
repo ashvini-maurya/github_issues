3344490e3271852bb48eb81c98e72d43552516ea
ashvinikumar <ashvinikumar45@gmail.com>
2016-05-02 15:45:33 +0530

    Added a Procfile

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ashvini/Desktop/github_issues/venv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/lib/python2.7/locale.py
@@ -0,0 +1,1 @@
+/home/ashvini/.pyenv/versions/2.7.9/lib/python2.7/locale.py
