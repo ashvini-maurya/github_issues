c668affb9d4d35773ffd91b7d90d10a5fbf46342
ashvinikumar <ashvinikumar45@gmail.com>
2016-05-21 00:52:52 +0530

    extended base.html to about.html

--- issues/views.py
+++ issues/views.py
@@ -10,7 +10,7 @@ def index(request):
     context = RequestContext(request,
                            {'request': request,
                             'user': request.user})
-    return render_to_response('base.html', context_instance=context)
+    return render_to_response('issues/index.html', context_instance=context)
 
 def getUrl(request):
     geturl = request.GET['URL']
@@ -19,7 +19,6 @@ def getUrl(request):
 
     if r.status_code == 200:
         total_issues_api = "https://api.github.com/repos/" + str(splitted_url[3]) + "/" + str(splitted_url[4])
-        #print total_issues_api
         total_issues_response = requests.get(total_issues_api)
         print total_issues_response
         total_issues_json = total_issues_response.json()
@@ -32,40 +31,45 @@ def getUrl(request):
         issue_api_in_last_24hr = "https://api.github.com/repos/" + str(splitted_url[3]) + "/" + str(splitted_url[4]) + "/issues?"
         get_url_api_data = requests.get(issue_api_in_last_24hr, params=parameters)
         last_24hr_url_api = get_url_api_data.url
-        print last_24hr_url_api
         last_24hr_url_api_data = requests.get(last_24hr_url_api)
-        print(last_24hr_url_api_data)
 
         li_last_24hr_url_api = []
         for api_urls in last_24hr_url_api_data.json():
             li_last_24hr_url_api.append(api_urls["html_url"])
 
-
         title_li = []
         for title in last_24hr_url_api_data.json():
             title_li.append(title['title'])
-        #print(title_li)
-
 
         dict_of_url_and_title = dict(zip(li_last_24hr_url_api, title_li))
-        #print dict_of_url_and_title
 
         count = 0
         for updated_value in get_url_api_data.json():
             count += 1
 
-        #print "last 24hr: ", count
-
         last_7days = last_24hr - timedelta(days=7)
         parameter2 = {'since': last_7days}
         issue_api_in_less_than_7days_but_greater_than_24hr = "https://api.github.com/repos/" + str(splitted_url[3]) + "/" + str(splitted_url[4]) + "/issues?"
         response_data = requests.get(issue_api_in_less_than_7days_but_greater_than_24hr, params=parameter2)
+        less_than_7days_ago_api = response_data.url
+        less_than_7days_ago_api_data = requests.get(less_than_7days_ago_api)
+
+        li_less_than_7days_url_api = []
+        for api_url in less_than_7days_ago_api_data.json():
+            li_less_than_7days_url_api.append(api_url["html_url"])
+
+        title_li2 = []
+        for title in less_than_7days_ago_api_data.json():
+            title_li2.append(title['title'])
+
+        dict_of_url_and_title2 = dict(zip(li_less_than_7days_url_api, title_li2))
+
         count2 = 0
         for _value in response_data.json():
             count2 += 1
 
         total_issues_greater_than_7days = total_issues_json["open_issues"] - count - count2
-        params = {'total': total_issues, 'count': count, 'count2': count2, 'total_greater_than_7days': total_issues_greater_than_7days, 'dict_of_url_and_title': dict_of_url_and_title}
+        params = {'total': total_issues, 'count': count, 'count2': count2, 'total_greater_than_7days': total_issues_greater_than_7days, 'dict_of_url_and_title': dict_of_url_and_title, 'dict_of_url_and_title2': dict_of_url_and_title2}
         return render(request, 'issues/getData.html', params)
 
 
